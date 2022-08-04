bbe04a31160369dcadae301e9190149147b1ee8e
Raf Ellinitakis <nickpap556@gmail.com>
2022-08-04 14:23:08 +0300

    new "smoothing"

--- serial_piezo_in.ino
+++ serial_piezo_in.ino
@@ -1,4 +1,6 @@
-const int vibrSensor = A0; // the piezo is connected to analog pin 0
+#include "avdweb_AnalogReadFast.h"
+
+const uint8_t vibrSensor = A0; // the piezo is connected to analog pin 0
 const int threshold = 5;  // threshold value to decide when the detected sound is a knock or not
 
 int elapsedTime = 0;
@@ -7,71 +9,53 @@ int currentTime=0;
 
 uint16_t peakCounter = 0;
 
-uint16_t packetTime = 500;  // the time window of a packet. Used for better serial visualisation
+uint16_t packetTime = 50;  // the time window of a packet. Used for better serial visualisation
 
 // these variables will change:
-uint16_t sensorReading = 0;      // variable to store the value read from the sensor pin
+uint16_t sensorValue = 0;      // variable to store the value read from the sensor pin
+long unsigned sensorReading;
 
-// next are the variables for smoothing
-const uint8_t numReadings = 5;
 
-uint16_t readings[numReadings];      // the readings from the analog input
-uint16_t readIndex = 0;              // the index of the current reading
-int total = 0;                  // the running total
-uint16_t average = 0;                // the average
+// smoothing
+int counter = 0;
 
-uint8_t inputPin = A0;
-
-unsigned long startTime2;
-unsigned long elapsedTime2;
 
 void setup() {
   Serial.begin(9600);       // use the serial port
-  // initialize all the readings to 0:
-  for (int thisReading = 0; thisReading < numReadings; thisReading++) {
-    readings[thisReading] = 0;
-  }
 
   analogReference(INTERNAL1V1); // THIS is ONLY for Arduino MEGA! It sets the reference AREF at 1.1V(resolution of 1mv)
 }
 
 void loop() {
-  startTime2=millis();
-  
-  sensorReading = analogRead(vibrSensor);
-  if(sensorReading>1){
+
+  sensorReading = analogReadFast(vibrSensor);
+  if(sensorReading>threshold){
     startTime = millis();
     while(elapsedTime<packetTime){
-      sensorReading = smoothingFunc();
-      
-      //Serial.println(sensorReading);
-      
+      sensorReading += analogReadFast(vibrSensor);
       elapsedTime = millis()-startTime;      
-    }    
+      counter ++;
+    }   
+
+    Serial.println(sensorReading/counter);
+    counter = 0;
     elapsedTime = 0;
   }
-  elapsedTime2 = millis()-startTime2;
-  Serial.println(elapsedTime2);
+  
 }
 
-
-int smoothingFunc(){
-  // subtract the last reading:
-  total = total - readings[readIndex];
-  // read from the sensor:
-  readings[readIndex] = analogRead(vibrSensor);
-  // add the reading to the total:
-  total = total + readings[readIndex];
-  // advance to the next position in the array:
-  readIndex = readIndex + 1;
-
-  // if we're at the end of the array...
-  if (readIndex >= numReadings) {
-    // ...wrap around to the beginning:
-    readIndex = 0;
+/*
+int smoothRead(int pin){
+  sensorValue = analogReadFast(pin);
+  if(counter<5){
+    smoothedValue = (sensorValue+99*lastValue)/200;
+    lastValue = sensorValue;
+  }
+  else{
+    counter = 0;
+    smoothedValue = lastValue = sensorValue;
   }
 
-  // calculate the average:
-  average = total / numReadings;
-  return average;
+  return smoothedValue;
 }
+*/
