433c7410fe964815feee475b8a208922522c4e20
Raf Ellinitakis <nickpap556@gmail.com>
2022-08-10 16:35:38 +0300

    rewrote the code for the raspi pico. Not Functional!

--- serial_piezo_in.ino
+++ serial_piezo_in.ino
@@ -1,61 +1,54 @@
-#include "avdweb_AnalogReadFast.h"
+const int vibrSensor = A0; // the piezo is connected to analog pin 0
+const int threshold = 30;  // threshold value to decide when the detected sound is a knock or not
 
-const uint8_t vibrSensor = A0; // the piezo is connected to analog pin 0
-const int threshold = 5;  // threshold value to decide when the detected sound is a knock or not
+int lastReadings[10];
+int sensorReading;
 
-int elapsedTime = 0;
-int startTime= 0;
-int currentTime=0;
-
-uint16_t peakCounter = 0;
-
-uint16_t packetTime = 50;  // the time window of a packet. Used for better serial visualisation
-
-// these variables will change:
-uint16_t sensorValue = 0;      // variable to store the value read from the sensor pin
-long unsigned sensorReading;
+// those need to be the same as in the tranceiver
+// they need to bee 64-bit unsigned to accept time values
+uint64_t const timeFactor = 40;
+uint64_t globalInterval = 2 * timeFactor; // duration of tones
+uint64_t globalPause = 0.5 * timeFactor; // duration between tones
 
 
-// smoothing
+uint64_t startTime;
+uint64_t currentTime;
+uint64_t lastSampleTime;
 int counter = 0;
-
+int const resolution = 100;  // how many readings per bit
+int measurements[resolution];  // holds all the measurements of one bit time window(to be later averaged)
 
 void setup() {
   Serial.begin(9600);       // use the serial port
-
-  analogReference(INTERNAL1V1); // THIS is ONLY for Arduino MEGA! It sets the reference AREF at 1.1V(resolution of 1mv)
 }
 
 void loop() {
+  sensorReading = analogRead(vibrSensor);
+  Serial.println(sensorReading);
 
-  sensorReading = analogReadFast(vibrSensor);
-  if(sensorReading>threshold){
-    startTime = millis();
-    while(elapsedTime<packetTime){
-      sensorReading += analogReadFast(vibrSensor);
-      elapsedTime = millis()-startTime;      
-      counter ++;
-    }   
 
-    Serial.println(sensorReading/counter);
-    counter = 0;
-    elapsedTime = 0;
+
+  // demodulation
+
+  if (sensorReading > threshold) { // the message has started
+    // sample for globalInterval*2 to see if it is an actual starting tone
+    currentTime = startTime = get_absolute_time()._private_us_since_boot;  // it returns a struct. See types.h of Pico
+    while (lastSampleTime = currentTime < startTime + globalInterval * 2) {
+      //sample the waveform 100 times in globalInterval*2 useconds
+      if (absolute_time_diff_us(currentTime - lastSampleTime) < (globalInterval * 2) / 100) {
+        sensorReading = analogRead(vibrSensor);
+        measurements[counter] = sensorReading; counter +=1;
+
+      }
+      currentTime = get_absolute_time_us()._private_us_since_boot;
+    }
+
+    // first 8 bits are the length of the payload. Each bit lasts globalInterval + globalPause seconds
+
+
   }
-  
+  //delay(1);
+
 }
 
-/*
-int smoothRead(int pin){
-  sensorValue = analogReadFast(pin);
-  if(counter<5){
-    smoothedValue = (sensorValue+99*lastValue)/200;
-    lastValue = sensorValue;
-  }
-  else{
-    counter = 0;
-    smoothedValue = lastValue = sensorValue;
-  }
 
-  return smoothedValue;
-}
-*/
